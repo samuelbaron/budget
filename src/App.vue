56c294e1db4f31407f224f51f195770a318262c5
samuel <samuel.baron.pl@gmail.com>
2023-09-10 15:46:16 +0200

    initial ui logic of auth components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
 <template>
-  <ion-app>
+  <ion-app v-if="authenticatedUser">
     <ion-router-outlet />
   </ion-app>
+  <div v-else>
+    <ContentWrapper>
+      <template v-slot:content>
+        <WelcomeView
+          v-if="showWelcomeView"
+          @login-request="(showLoginForm = true), (showWelcomeView = false)"
+          @registration-request="
+            (showRegistrationForm = true), (showWelcomeView = false)
+          "
+        />
+        <LoginForm v-if="showLoginForm" />
+        <RegistrationForm v-if="showRegistrationForm" />
+      </template>
+    </ContentWrapper>
+  </div>
 </template>
 <script lang="ts">
 import {
@@ -11,16 +26,32 @@ import {
   IonContent,
   IonRouterOutlet,
 } from "@ionic/vue";
+import WelcomeView from "@/components/authentication/WelcomeView.vue";
+import LoginForm from "@/components/authentication/LoginForm.vue";
+import ContentWrapper from "@/components/ui/ContentWrapper.vue";
+import RegistrationForm from "@/components/authentication/RegistrationForm.vue";
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: "App",
   components: {
     IonApp,
     IonToolbar,
     IonPage,
     IonContent,
     IonRouterOutlet,
+    WelcomeView,
+    LoginForm,
+    RegistrationForm,
+    ContentWrapper,
   },
+  data() {
+    return {
+      authenticatedUser: false,
+      showWelcomeView: true,
+      showLoginForm: false,
+      showRegistrationForm: false,
+    };
+  },
+  name: "App",
 });
 </script>
