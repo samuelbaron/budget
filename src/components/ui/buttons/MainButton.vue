7f26cea12ff08a1d16cceef120d7b7bcf7164b0a
samuel <samuel.baron.pl@gmail.com>
2023-09-10 15:45:11 +0200

    created button component

---
+++ src/components/ui/buttons/MainButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button :class="computedClass">{{ text }}</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { ButtonColor } from "@/mixins/types";
+export default defineComponent({
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    color: {
+      type: String as () => ButtonColor,
+      required: true,
+    },
+  },
+  computed: {
+    computedClass() {
+      return [this.color, "rounded-lg", "p-3", "text-white", "font-medium"];
+    },
+  },
+});
+</script>
