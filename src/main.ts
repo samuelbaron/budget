41a51385629f81dd68a1e72eff87927c4f106ab6
samuel <samuel.baron.pl@gmail.com>
2023-09-10 15:48:29 +0200

    uncommented a lew lines in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,10 @@ import router from "./router";
 
 import { IonicVue } from "@ionic/vue";
 
-/* Core CSS required for Ionic components to work properly */
 import "@ionic/vue/css/core.css";
-
-/* Basic CSS for apps built with Ionic */
 import "@ionic/vue/css/normalize.css";
 import "@ionic/vue/css/structure.css";
 import "@ionic/vue/css/typography.css";
-
-/* Optional CSS utils that can be commented out */
 import "@ionic/vue/css/padding.css";
 import "@ionic/vue/css/float-elements.css";
 import "@ionic/vue/css/text-alignment.css";
